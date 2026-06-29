1fc9a62dad4723ba2cf03a8fc58a1603ac80759f
Pratham Masurkar <pratham.masurkar23@spit.ac.in>
2026-06-29 14:01:12 +0530

    initial commit

--- src/controllers/ml.controller.js
+++ src/controllers/ml.controller.js
@@ -1,8 +1,15 @@
 exports.receiveViolation = async (req, res) => {
 
+    console.log("Body:");
+    console.log(req.body);
+
+    console.log("Files:");
+    console.log(req.files);
+
     return res.status(200).json({
         success: true,
-        message: "ML endpoint working"
+        body: req.body,
+        files: Object.keys(req.files || {})
     });
 
 };
--- src/routes/ml.routes.js
+++ src/routes/ml.routes.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const multer = require("multer");
+const mlController = require("../controllers/ml.controller");
 
 const router = express.Router();
 
-const mlController = require("../controllers/ml.controller");
-router.post("/violations", mlController.receiveViolation);
+const upload = multer({
+    storage: multer.memoryStorage(),
+});
+
+router.post(
+    "/violations",
+    upload.fields([
+        { name: "image", maxCount: 1 },
+        { name: "plateCrop", maxCount: 1 }
+    ]),
+    mlController.receiveViolation
+);
 
 module.exports = router;
